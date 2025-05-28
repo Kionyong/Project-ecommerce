45fa479e39b17b3b6d76e39a0bb02daf073fbeb6
kionyong <kionyong@2gmail.com>
2025-05-28 13:44:20 +0700

    edit file ragister.php

--- ragister.php
+++ ragister.php
@@ -26,7 +26,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ragister</title>
-    <link rel="stylesheet" href="./style.css">
 </head>
 <style>
     body {
